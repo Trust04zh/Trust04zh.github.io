74719e886395a3e89db6b30e5db11a360f9be39b
Trust04zh <12012624@mail.sustech.edu.cn>
2021-08-03 16:01:06 +0800

    Site updated: 2021-08-03 16:01:06

--- js/fix_picture_path_on_window_loaded_with_prepend_root.js
+++ js/fix_picture_path_on_window_loaded_with_prepend_root.js
@@ -2,16 +2,20 @@ window.addEventListener('load', (event) => {
 	var picture_render_nodes = document.querySelectorAll('.fancybox');
 	for(var i = 0; i < picture_render_nodes.length; i++) {
 		var href = picture_render_nodes[i].href;
-		var reg = /(http[s]{0,1}:\/\/.*?\/)(.*)(\/.*?assets.*)/g;
-		var result = href.replace(reg, '$1_posts$3' );
-		picture_render_nodes[i].href = result;
+		if(href.indexOf('_posts') == -1) {
+			var reg = /(http[s]{0,1}:\/\/)(.*?)\/(.*)/g;
+			var result = href.replace(reg, '/_posts/$3' );
+			picture_render_nodes[i].href = result;
+		}
 	}
 	
 	for(var i = 0; i < picture_render_nodes.length; i++) {
 		var picture_node = picture_render_nodes[i].firstChild;
 		var href = picture_node.src;
-		var reg = /(http[s]{0,1}:\/\/.*?\/)(.*)(\/.*?assets.*)/g;
-		var result = href.replace(reg, '$1_posts$3' );
-		picture_node.src = result;
+		if(href.indexOf('_posts') == -1) {
+			var reg = /(http[s]{0,1}:\/\/)(.*?)\/(.*)/g;
+			var result = href.replace(reg, '/_posts/$3' );
+			picture_node.src = result;
+		}
 	}
 });
